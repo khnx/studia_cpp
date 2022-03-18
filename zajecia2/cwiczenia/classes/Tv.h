f4196b31764d2621d6efac1b25e6bf6a86b263d5
khnx <radoslaw.sm.tr@gmail.com>
2022-03-18 17:25:15 +0100

    update

---
+++ zajecia2/cwiczenia/classes/Tv.h
@@ -0,0 +1,6 @@
+class Tv {
+private:
+
+public:
+
+};
--- zajecia2/cwiczenia/main.cpp
+++ zajecia2/cwiczenia/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstdlib>
 
+#include "classes/Tv.h"
+
 int main() {
   
   std::cout << "Hello World!" << std::endl;
